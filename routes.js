0e8772638484bed3416998d79d8dcca9e7dc186d
Todd Oquist <toquist@gmail.com>
2013-12-12 17:30:41 -0800

    starting to build out APIs

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 var User = require('./models');
 
-module.exports = function(app, passport) {
-	app.get('/', function(req, res){
+module.exports = function (app, passport) {
+	app.get('/', function (req, res) {
 		if (req.isAuthenticated()) {
 			res.render('index', {user: req.user});
 		} else {
@@ -9,25 +9,55 @@ module.exports = function(app, passport) {
 		}
 	});
 
-	app.get('/login', function(req, res){ 
+	app.get('/login', function (req, res) { 
 		res.render('login');
 	});
 
 	app.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login'}));
+
+	app.get('/logout', function (req, res) {
+		req.logout();
+		res.redirect('/');
+	});
 
 	app.get('/signup', function (req, res) {
 		res.render('signup');
 	});
 
 	app.post('/signup', function (req, res, next) {
-		User.signup(req.body.email, req.body.password, function(err, user){
-			if (err) {throw err;}
+		User.signup(req.body.email, req.body.password, function (err, user) {
+			if (err) { return res.render('signup'); }
 			
-			req.login(user, function(err) {
-				if (err) {return next(err);}
-				
+			req.login(user, function (err) {
+				if (err) return next(err);
 				return res.redirect('/');
 			});
+		});
+	});
+
+	app.get('/test_api', function (req, res) {
+		if (req.isAuthenticated()) {
+			res.render('testApi', {user: req.user});
+		} else {
+			res.redirect('/')
+		}
+	});
+
+	app.post('/api/users/:id/spin', function (req, res, next) {
+		if (!req.isAuthenticated()) return res.send(401);
+
+		req.user.spin(req.body.lines, req.body.bet, function (err, results) {
+			console.log(results);
+		});
+	});
+
+	app.post('/api/users/:id/add_credits', function (req, res, next) {
+		if (!req.isAuthenticated()) return res.send(401);
+
+		req.user.addCredits(req.body.credits, function (err, numAffected){
+			if (err) return res.send(500);
+
+			res.json({credits: req.user.get('credits')});
 		});
 	});
 };
